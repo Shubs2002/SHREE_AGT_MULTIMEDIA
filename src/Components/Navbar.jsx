a96190e83f11b7c96cb6a2c3242ddf5f9928d33d
Shubs2002 <s.d.nakashe2002@gmail.com>
2025-03-05 13:10:10 +0530

    Bugs resolved

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
       <ul className={isOpen ? "nav-links open" : "nav-links"}>
         <li><a href="#hero">Home</a></li>
         <li><a href="#features">Features</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="#features">Contact</a></li>
       </ul>
     </nav>
   );
